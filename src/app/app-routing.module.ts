6040266fdb8e26d7cd5808f0859168383456909b
ankitsaini345@gmail.com <ankitsaini345@gmail.com>
2019-10-10 03:36:42 +0530

    usehash

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,18 +8,18 @@ import { InitiativesComponent } from './initiatives/initiatives.component';
 const routes: Routes = [
   { path: 'home', loadChildren: './dashboard/dashboard.module#DashboardModule' },
   { path: 'login', loadChildren: './login/login.module#LoginModule' },
-  { path : 'fund-transfer', component : FundTransferComponent},
-  { path : 'initiatives', component : InitiativesComponent},
-  { path : 'user-profile', component : UserProfileComponent},
+  { path: 'fund-transfer', component: FundTransferComponent },
+  { path: 'initiatives', component: InitiativesComponent },
+  { path: 'user-profile', component: UserProfileComponent },
   { path: 'Register', loadChildren: './register/register.module#RegisterModule' },
   { path: 'open-initiatives', loadChildren: './open-initiatives/open-initiatives.module#OpenInitiativesModule' },
   { path: 'idea-submission', loadChildren: './idea-submission/idea-submission.module#IdeaSubmissionModule' },
   { path: 'initiative', component: InitiativesComponent },
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: '', redirectTo: 'home', pathMatch: 'full' }
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { useHash: true })],
   exports: [RouterModule],
 })
 export class AppRoutingModule { }
