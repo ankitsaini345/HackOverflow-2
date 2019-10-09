159587b22d55aa00b8eec4f24d134d640e61438a
ankitsaini345@gmail.com <ankitsaini345@gmail.com>
2019-10-10 03:30:02 +0530

    test2

--- index.js
+++ index.js
@@ -8,8 +8,8 @@ const app = express();
 app.use(express.static(__dirname + '/dist/SampleApp'));
 
 app.get('/*', function(req,res) {
-
-res.sendFile(__dirname+'/dist/SampleApp/index.html');
+res.json(__dirname);
+// res.sendFile(__dirname+'/dist/SampleApp/index.html');
 });
 
 // Start the app by listening on the default Heroku port
